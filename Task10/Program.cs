77ef2db9c7f75d56d1ba281035dc968119b0b8f5
Irina <rennersha@mail.ru>
2023-06-20 18:42:44 +0300

    Добавила файл Program.cs

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,3 +3,17 @@
 782 -> 8
 918 -> 1 */
 
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+
+if (Length == 3) 
+{
+	int result = (Value/10) % 10;
+	Console.WriteLine("Вторая цифра -> " + result);
+} 
+else 
+{
+	Console.WriteLine("Ошибка не трёхзначное число");
+}
